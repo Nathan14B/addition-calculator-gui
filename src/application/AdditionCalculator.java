64c0108cf6e2009eacd3b36f777d3e722fd60300
Nathan14B <nathanadnerbueno2002@gmail.com>
2022-04-01 18:47:14 -0300

    Class AdditionCalculator implemented

--- src/application/AdditionCalculator.java
+++ src/application/AdditionCalculator.java
@@ -1,10 +1,33 @@
 package application;
 
+import javax.swing.JOptionPane;
+
 public class AdditionCalculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String firstNumberInput;
+		do {
+			firstNumberInput = JOptionPane.showInputDialog("Enter first integer:");
+			
+			if (!firstNumberInput.matches("-?\\d+")) {
+				JOptionPane.showMessageDialog(null, "Invalid value! Please enter only integers!");
+			}
+		} while(!firstNumberInput.matches("-?\\d+"));
+		
+		String secondNumberInput;
+		do {
+			secondNumberInput = JOptionPane.showInputDialog("Enter second integer:");
+			
+			if (!secondNumberInput.matches("-?\\d+")) {
+				JOptionPane.showMessageDialog(null, "Invalid value! Please enter only integers!");
+			}
+		} while(!secondNumberInput.matches("-?\\d+"));
+		
+		int firstSummand = Integer.parseInt(firstNumberInput);
+		int secondSummand = Integer.parseInt(secondNumberInput);
+		int sum = firstSummand + secondSummand;
+		
+		String message = String.format("%d plus %d equals %d", firstSummand, secondSummand, sum);
+		JOptionPane.showMessageDialog(null, message);
 	}
-
 }
